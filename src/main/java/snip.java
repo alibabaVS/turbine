459ffb27475e89513aeb29ac7507c63a626a2bd8
hui <774877338@qq.com>
2018-08-10 09:31:15 +0800

    初始导入

---
+++ src/main/java/snip.java
@@ -0,0 +1,12 @@
+/**
+ * PackageName:PACKAGE_NAME
+ * ClassName: snip
+ *
+ * @author zha.jiangjiang
+ * @Description:
+ * @Date: 2018/8/10 9:29
+ * @see
+ * @since JDK 1.8
+ */
+public class snip {
+}
